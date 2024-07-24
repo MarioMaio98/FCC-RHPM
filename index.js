0697a59048b933ba2a43d13ce9fd65c8a4f7e849
Mario maio <mariomaio92@gmail.com>
2024-07-24 10:10:03 +0000

    my exercise

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+class WhoamiController {
+  static getWhoami(req, res) {
+    res.json({
+      ipaddress: req.socket.remoteAddress,
+      language: req.headers['accept-language'],
+      software: req.headers['user-agent']
+    });
+  }
+}
+
+app.get('/api/whoami', WhoamiController.getWhoami);
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
